2da071f6d4453483a5358cb8b213c28115ccb35b
Shreeram Aute <33367448+shreeram09@users.noreply.github.com>
2023-02-17 14:59:20 +0000

    db connection and container config updated

---
+++ database/database.js
@@ -0,0 +1,16 @@
+const db = require('mongoose')
+
+const {MONGODB_URL}=process.env
+//mongodb+srv://shreeram09:<password>@cluster0.lsdsvyg.mongodb.net/test
+exports.connect = ()=>{
+    db.connect(MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then()
+    .catch((err)=>{
+        console.log(`db connection failed`);
+        console.log(err);
+        process.exit(1)
+    })
+}
